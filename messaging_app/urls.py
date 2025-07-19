ac45c5bddf463e775a9157d1230ee3b1afb59b61
Therushmorare <tshego.tt.morare@gmail.com>
2025-07-19 21:30:49 +0000

    fixed urls

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('messaging_app.chats.urls')),  # Adjust this path as needed
+    path('api/', include('messaging_app.chats.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
