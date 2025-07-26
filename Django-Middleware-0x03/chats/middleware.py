0b593b2a3ba3ffe39b7bd9948722682363ead247
Therushmorare <tshego.tt.morare@gmail.com>
2025-07-26 17:08:10 +0000

    middleware fix

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,18 +1,16 @@
-# chats/middleware.py
-
 from datetime import datetime
+import logging
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.logger = logging.getLogger(__name__)
+        self.handler = logging.FileHandler('requests.log')
+        self.logger.addHandler(self.handler)
 
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else 'Anonymous'
-        log_line = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
-        
-        with open("requests.log", "a") as log_file:
-            log_file.write(log_line)
-
-        response = self.get_response(request)
-        return response
+        log_entry = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
+        self.logger.warning(log_entry)
+        return self.get_response(request)
 
