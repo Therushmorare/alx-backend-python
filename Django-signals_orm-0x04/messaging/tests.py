f73758bc01ad42772899e06cff65ee028e0eb99d
Therushmorare <tshego.tt.morare@gmail.com>
2025-08-02 18:44:37 +0000

    task 0

---
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
+
+class NotificationSignalTest(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username='sender', password='pass')
+        self.receiver = User.objects.create_user(username='receiver', password='pass')
+
+    def test_notification_created_on_message(self):
+        Message.objects.create(sender=self.sender, receiver=self.receiver, content="Hello!")
+        self.assertEqual(Notification.objects.count(), 1)
+        notif = Notification.objects.first()
+        self.assertEqual(notif.user, self.receiver)
